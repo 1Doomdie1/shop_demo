367431d0c721cc2f7a613956ebc49ea855feb8f8
Foo Barrymore <98619759+foobarrymore@users.noreply.github.com>
2022-03-24 17:40:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from products_categories import *
 
 
 def main():
-#     cl1_addr = Address('London', 12345, 'London Bridge', 1, 12, 1, 20)
-#     cl1 = Client(61253, 'Doomdie', cl1_addr)
+    cl1_addr = Address('London', 12345, 'London Bridge', 1, 12, 1, 20)
+    cl1 = Client(61253, 'Doomdie', cl1_addr)
 
-    cl2_addr = Address('London', 12345, 'London Bridge', 1, 12, 1, 20)
-    cl2 = Client(1234, 'Doomdie', cl2_addr)
+#     cl2_addr = Address('London', 12345, 'London Bridge', 1, 12, 1, 20)
+#     cl2 = Client(1234, 'Doomdie', cl2_addr)
 
     milk = Milk('Milk', 10, 2345, '1.5L', 12, "12.12.2022")
     ice_cream = Ice_cream('Vanila Ice cream', 15, 5654, '300g', '12.12.2022')
