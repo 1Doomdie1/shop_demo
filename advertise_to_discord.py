3aa3ca8bf16d1d1215447c06e1621a1ed5845c11
Doomdie <43732125+1Doomdie1@users.noreply.github.com>
2022-07-30 01:28:48 +0300

    Update advertise_to_discord.py

--- advertise_to_discord.py
+++ advertise_to_discord.py
@@ -2,14 +2,12 @@ import requests
 from sys import argv
 
 
-PR = argv[6]
 PR_TITLE = argv[5]
 PR_AUTHOR = argv[4]
 PR_NUMBER = argv[3]
 DISCORD_CHANEL = argv[2]
 DISCORD_TOKEN = argv[1]
 
-print(PR)
 data = {"content": f"PR: {PR_TITLE}\nLINK: https://github.com/unifyai/ivy/pull/{PR_NUMBER}\nAUTHOR:{PR_AUTHOR}\nSTATUS: reviewed"}
 header = {"authorization": DISCORD_TOKEN}
 requests.post(DISCORD_CHANEL, data=data, headers=header)
